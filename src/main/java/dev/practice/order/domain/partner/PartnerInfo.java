49ded8e1110130159f2583456055888b0848f923
jaemin <rkwoals52497@gmail.com>
2022-07-22 10:08:58 +0900

    파트너 정보 저장

--- src/main/java/dev/practice/order/domain/partner/PartnerInfo.java
+++ src/main/java/dev/practice/order/domain/partner/PartnerInfo.java
@@ -1,8 +1,9 @@
 package dev.practice.order.domain.partner;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Getter @NoArgsConstructor
 public class PartnerInfo {
     private Long id;
     private String partnerToken;
@@ -10,4 +11,23 @@ public class PartnerInfo {
     private String businessNo;
     private String email;
     private Partner.Status status;
+
+    public PartnerInfo(Long id, String partnerToken, String partnerName, String businessNo, String email, Partner.Status status) {
+        this.id = id;
+        this.partnerToken = partnerToken;
+        this.partnerName = partnerName;
+        this.businessNo = businessNo;
+        this.email = email;
+        this.status = status;
+    }
+
+    public static PartnerInfo of(Partner partner) {
+        return new PartnerInfo(
+                partner.getId(),
+                partner.getPartnerToken(),
+                partner.getPartnerName(),
+                partner.getBusinessNo(),
+                partner.getEmail(),
+                partner.getStatus());
+    }
 }
--- src/main/java/dev/practice/order/domain/partner/PartnerService.java
+++ src/main/java/dev/practice/order/domain/partner/PartnerService.java
@@ -3,7 +3,7 @@ package dev.practice.order.domain.partner;
 public interface PartnerService {
     // req: Command, Criteria // res: Info
 
-    PartnerInfo registerPartner(PartnerCommand partnerCommand);
+    PartnerInfo registerPartner(PartnerCommand command);
     PartnerInfo getPartnerInfo(String partnerToken);
     PartnerInfo enablePartner(String partnerToken);
     PartnerInfo disablePartner(String partnerToken);
